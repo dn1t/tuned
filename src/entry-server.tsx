eba65b23156163fdd2d9df14f2de4edc938b859d
dn1t <me@tica.fun>
2024-06-20 00:36:10 +0900

    moved dark mode script to head

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -6,6 +6,7 @@ export default createHandler(() => (
     document={({ assets, children, scripts }) => (
       <html lang='ko'>
         <head>
+          <script innerHTML="(()=>{const t=localStorage.getItem('theme');if(t==='dark'||t==='light')document.documentElement.dataset.theme=t;else document.documentElement.dataset.theme=matchMedia('(prefers-color-scheme:dark)').matches?'dark':'light'})()" />
           <meta charset='utf-8' />
           <meta name='viewport' content='width=device-width, initial-scale=1' />
           <link rel='icon' href='/favicon.ico' />
@@ -14,7 +15,6 @@ export default createHandler(() => (
         <body>
           <div id='app'>{children}</div>
           {scripts}
-          <script innerHTML="(()=>{const t=localStorage.getItem('theme');if(t==='dark'||t==='light')document.documentElement.dataset.theme=t;else document.documentElement.dataset.theme=matchMedia('(prefers-color-scheme:dark)').matches?'dark':'light'})()" />
         </body>
       </html>
     )}
