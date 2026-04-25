ef0fe7da2828ec3bf6635a89ac78fe7ea9aa6baf
David Lee <me@tica.fun>
2026-04-26 00:03:59 +0900

    reset project

--- apps/web-old/app.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "@solidjs/start/config";
-import tailwindcss from "@tailwindcss/vite";
-import icons from "unplugin-icons/vite";
-
-defineConfig({
-  vite: {
-    plugins: [tailwindcss(), icons({ compiler: "solid" })],
-  },
-});
